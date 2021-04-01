e9f39560b156452ac7386a431ca1dfbd348c1d62
Willian Jesus da Silva <46569232+Willianjesusdasilva@users.noreply.github.com>
2021-04-01 13:26:52 -0300

    Update abstract_api.py

--- abstract_api.py
+++ abstract_api.py
@@ -1,22 +1,22 @@
 import matplotlib.pyplot as plt
-from colorizers import *
+from Video_colorizer.colorizers import *
 import numpy as np
 
 
 def colorize_frame(img):
-        colorizer_eccv16 = eccv16(pretrained=True).eval()
-        #colorizer_siggraph17 = siggraph17(pretrained=True).eval()
-        if(False):
-                colorizer_eccv16.cuda()
-                #colorizer_siggraph17.cuda()
+        #colorizer_eccv16 = eccv16(pretrained=True).eval()
+        colorizer_siggraph17 = siggraph17(pretrained=True).eval()
+        if(True):
+                #colorizer_eccv16.cuda()
+                colorizer_siggraph17.cuda()
         img = load_img(img)
         (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
-        if(False):
+        if(True):
                 tens_l_rs = tens_l_rs.cuda()
 
         img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig),dim=1))
-        out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
-        #out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
+        #out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
+        out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
 
-        return out_img_eccv16
-        #return out_img_siggraph17
+        #return out_img_eccv16
+        return out_img_siggraph17
